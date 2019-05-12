310b731aef2a4461f8daa8098f8747e38faf176b
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-05-12 11:30:08 -0300

    ObterSaldo e DefinirSaldo

--- Formações em Programação/dotNET/C# parte 2 Introdução à Orientação a Objetos/ByteBank/06-ByteBank/ContaCorrente.cs
+++ Formações em Programação/dotNET/C# parte 2 Introdução à Orientação a Objetos/ByteBank/06-ByteBank/ContaCorrente.cs
@@ -11,28 +11,39 @@ namespace _06_ByteBank
         public Cliente titular;
         public int agencia;
         public int numero;
-        public double saldo = 100;
+        private double saldo = 100;
+
+
+        public double ObterSaldo() => saldo;
+
+        public void DefinirSaldo(double saldo)
+        {
+            if (saldo < 0)
+                return;
+            else
+                this.saldo = saldo;
+        }
 
         public bool Sacar(double valor)
         {
-            if (this.saldo < valor)
+            if (saldo < valor)
                 return false;
 
-            this.saldo -= valor;
+            saldo -= valor;
             return true;
         }
 
         public void Depositar(double valor)
         {
-            this.saldo += valor;
+            saldo += valor;
         }
 
         public bool Transferir(double valor, ContaCorrente contaDestino)
         {
-            if (this.saldo < valor)
+            if (saldo < valor)
                 return false;
 
-            this.saldo -= valor;
+            saldo -= valor;
             contaDestino.Depositar(valor);
             return true;
         }
