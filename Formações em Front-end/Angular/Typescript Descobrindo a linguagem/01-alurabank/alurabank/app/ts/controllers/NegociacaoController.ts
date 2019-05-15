9eede8e0a3debb85157cf01501fe17fb53047fb1
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-05-14 21:40:58 -0300

    Apresentando o modelo para o usuário

--- Formações em Front-end/Angular/Typescript Descobrindo a linguagem/01-alurabank/alurabank/app/ts/controllers/NegociacaoController.ts
+++ Formações em Front-end/Angular/Typescript Descobrindo a linguagem/01-alurabank/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -4,11 +4,13 @@ class NegociacaoController{
     private _inputValor: HTMLInputElement;
 
     private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor(){
         this._inputData = <HTMLInputElement>document.querySelector("#data");
         this._inputQuantidade = <HTMLInputElement>document.querySelector("#quantidade");
         this._inputValor = <HTMLInputElement>document.querySelector("#valor");
+        this._negociacoesView.update();
     }
 
     public adiciona(event: Event): void{
@@ -21,6 +23,6 @@ class NegociacaoController{
         );
         
         this._negociacoes.adiciona(negociacao);
-        
+
     }
 }
