d8a4839adc044d70515cf0ee8287c063eca60c0d
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-05-15 23:56:22 -0300

    Ativando strictNullChecks

--- Formações em Front-end/Angular/TypeScript parte 2 Mais técnicas e boas práticas/01-alurabank/alurabank/app/ts/models/negociacoes.ts
+++ Formações em Front-end/Angular/TypeScript parte 2 Mais técnicas e boas práticas/01-alurabank/alurabank/app/ts/models/negociacoes.ts
@@ -9,8 +9,7 @@ export class Negociacoes{
         this._negociacoes.push(negociacao);
     }
 
-    paraArray():Negociacao[] {
-
-        return [].concat(this._negociacoes);
+    paraArray(): Negociacao[] {
+        return ([] as Negociacao[]).concat(this._negociacoes);
     }
 }
