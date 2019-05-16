9c9f12808f696c35cc65d3a1ce2a421c9d1fe9ca
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-05-15 21:29:26 -0300

    O papel de um carregador de módulos

--- Formações em Front-end/Angular/TypeScript parte 2 Mais técnicas e boas práticas/01-alurabank/alurabank/app/js/controllers/NegociacaoController.js
+++ Formações em Front-end/Angular/TypeScript parte 2 Mais técnicas e boas práticas/01-alurabank/alurabank/app/js/controllers/NegociacaoController.js
@@ -1,22 +1,42 @@
-import { Negociacoes } from "../models/negociacoes";
-import { NegociacoesView } from "../views/NegociacoesView";
-import { MensagemView } from "../views/MensagemView";
-import { Negociacao } from "../models/negociacao";
-export class NegociacaoController {
-    constructor() {
-        this._negociacoes = new Negociacoes();
-        this._negociacoesView = new NegociacoesView('#negociacoesView');
-        this._mensagemView = new MensagemView('#mensagemView');
-        this._inputData = $("#data");
-        this._inputQuantidade = $("#quantidade");
-        this._inputValor = $("#valor");
-        this._negociacoesView.update(this._negociacoes);
-    }
-    adiciona(event) {
-        event.preventDefault();
-        const negociacao = new Negociacao(new Date(this._inputData.val().replace(/-/g, ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
-        this._negociacoes.adiciona(negociacao);
-        this._negociacoesView.update(this._negociacoes);
-        this._mensagemView.update("Negociação adicionada com sucesso!");
-    }
-}
+System.register(["../models/negociacoes", "../views/NegociacoesView", "../views/MensagemView", "../models/negociacao"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var negociacoes_1, NegociacoesView_1, MensagemView_1, negociacao_1, NegociacaoController;
+    return {
+        setters: [
+            function (negociacoes_1_1) {
+                negociacoes_1 = negociacoes_1_1;
+            },
+            function (NegociacoesView_1_1) {
+                NegociacoesView_1 = NegociacoesView_1_1;
+            },
+            function (MensagemView_1_1) {
+                MensagemView_1 = MensagemView_1_1;
+            },
+            function (negociacao_1_1) {
+                negociacao_1 = negociacao_1_1;
+            }
+        ],
+        execute: function () {
+            NegociacaoController = class NegociacaoController {
+                constructor() {
+                    this._negociacoes = new negociacoes_1.Negociacoes();
+                    this._negociacoesView = new NegociacoesView_1.NegociacoesView('#negociacoesView');
+                    this._mensagemView = new MensagemView_1.MensagemView('#mensagemView');
+                    this._inputData = $("#data");
+                    this._inputQuantidade = $("#quantidade");
+                    this._inputValor = $("#valor");
+                    this._negociacoesView.update(this._negociacoes);
+                }
+                adiciona(event) {
+                    event.preventDefault();
+                    const negociacao = new negociacao_1.Negociacao(new Date(this._inputData.val().replace(/-/g, ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
+                    this._negociacoes.adiciona(negociacao);
+                    this._negociacoesView.update(this._negociacoes);
+                    this._mensagemView.update("Negociação adicionada com sucesso!");
+                }
+            };
+            exports_1("NegociacaoController", NegociacaoController);
+        }
+    };
+});
