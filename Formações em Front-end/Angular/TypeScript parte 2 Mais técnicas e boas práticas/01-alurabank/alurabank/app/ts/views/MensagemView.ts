640408158176208c165e9a0d28053358c6e8490a
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-05-15 21:08:44 -0300

    Organização do código através de namespaces

---
+++ Formações em Front-end/Angular/TypeScript parte 2 Mais técnicas e boas práticas/01-alurabank/alurabank/app/ts/views/MensagemView.ts
@@ -0,0 +1,10 @@
+
+namespace Views{
+    export class MensagemView extends View<string> {
+
+        template(model: string): string{
+            return `<p class="alert alert-info">${model}</p>`;
+        }
+    
+    }
+}
