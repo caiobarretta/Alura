5381d09093b84b98c5ab5403111eb33ad6afc3da
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-05-26 13:24:21 -0300

    Conhecendo exceções

---
+++ Formações em Programação/dotNET/C# parte 4 Entendendo exceções/ByteBank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(423423, 35435);
+            Console.WriteLine(ContaCorrente.TaxaOperacao);
+            Console.ReadLine();
+        }
+    }
+}
