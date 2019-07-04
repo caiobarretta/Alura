053df50bb95cbb45d2b7609725f027de6c0cffb9
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-07-03 23:01:01 -0300

    Quantificadores.

--- Formações em Programação/dotNET/C# parte 6 Str, exp reg e class Obj/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ Formações em Programação/dotNET/C# parte 6 Str, exp reg e class Obj/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -20,7 +20,9 @@ namespace ByteBank.SistemaAgencia
 
 
 
-            string padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+            //string padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+            //string padrao = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+            string padrao = "[0-9]{4}[-][0-9]{4}";
             string textoDeTeste = "Meu nome é Guilherme, me ligue em 4784-4546";
 
             Match resultado = Regex.Match(textoDeTeste, padrao);
