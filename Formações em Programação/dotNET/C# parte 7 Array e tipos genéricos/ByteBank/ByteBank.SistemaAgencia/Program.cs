82b68d59b0ceaa2a9948597513efc22c09b7adef
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-07-04 23:11:48 -0300

    Iterando arrays e Length.

--- Formações em Programação/dotNET/C# parte 7 Array e tipos genéricos/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ Formações em Programação/dotNET/C# parte 7 Array e tipos genéricos/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -16,18 +16,23 @@ namespace ByteBank.SistemaAgencia
         {
             //ARRAY de inteiros, com 5 posições.
             //int[] idades = null;
-            int[] idades = new int[5];
+            int[] idades = new int[6];
 
             idades[0] = 15;
             idades[1] = 28;
             idades[2] = 35;
             idades[3] = 50;
             idades[4] = 28;
+            idades[5] = 60;
 
-            int indice = 4;
-            int idadeNoIndice4 = idades[indice];
+            int acumulador = 0;
+            for (int indice = 0; indice < idades.Length; indice++)
+            {
+                acumulador += idades[indice];
+            }
 
-            Console.WriteLine(idadeNoIndice4);
+            Console.WriteLine($"A média é: {acumulador / idades.Length}");
+
             Console.ReadLine();
         }
     }
