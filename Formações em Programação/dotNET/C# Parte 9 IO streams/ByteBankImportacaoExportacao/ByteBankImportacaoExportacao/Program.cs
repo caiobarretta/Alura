9f763820cdc778f1221def42f309e457b7bd4b6d
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-07-06 12:56:32 -0300

    Criando um FileStream.

---
+++ Formações em Programação/dotNET/C# Parte 9 IO streams/ByteBankImportacaoExportacao/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,43 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            var enderecoArquivo = "contas.txt";
+
+            var fluxoDoArquivo = new FileStream(enderecoArquivo, FileMode.Open);
+
+            var buffer = new byte[1024]; // 1kb
+            var numeroDeBytesLidos = -1;
+
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBufferNaTela(buffer);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void EscreverBufferNaTela(byte[] buffer)
+        {
+            foreach (var meuByte in buffer)
+            {
+                Console.Write(meuByte);
+                Console.Write(" ");
+            }
+                
+        }
+    }
+} 
+ 
