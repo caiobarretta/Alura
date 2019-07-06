7932a9468d4b48c364a77597cf1fbd8d09ee5516
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-07-06 15:50:00 -0300

    Criando arquivos CSV.

---
+++ Formações em Programação/dotNET/C# Parte 9 IO streams/ByteBankImportacaoExportacao/ByteBankImportacaoExportacao/3_CriandoArquivo.cs
@@ -0,0 +1,28 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void CriarArquivo()
+        {
+            var caminhoNovoArquivo = "ContasExportadas.csv";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            {
+                var contaComoString = "123,123132,0.50,Gustavo Santos";
+                var encoding = Encoding.UTF8;
+
+                var bytes = encoding.GetBytes(contaComoString);
+
+                fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
