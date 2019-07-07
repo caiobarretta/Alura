4f7ef6f9443f0f721b2e11fbb737f1bc05e28ac0
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-07-07 15:14:31 -0300

    Operações com Arrays.

--- Formações em Programação/C# Collections/Parte 1/CSharpCollections/CSharpCollections/Program.cs
+++ Formações em Programação/C# Collections/Parte 1/CSharpCollections/CSharpCollections/Program.cs
@@ -16,25 +16,66 @@ namespace CSharpCollections
             //{
             //    aulaIntro,
             //    aulaModelando,
+            //    aulaModelando,
             //    aulaSets
             //};
 
-            string[] aulas = new string[3];
+            string[] aulas = new string[4];
             aulas[0] = aulaIntro;
             aulas[1] = aulaModelando;
-            aulas[2] = aulaSets;
+            aulas[2] = aulaModelando;
+            aulas[3] = aulaSets;
 
             Imprimir(aulas);
 
             Console.WriteLine("-------------------------------");
             aulaIntro = "Trabalhando com Arrays";
-
-            Imprimir(aulas);
+            ImprimirFor(aulas);
 
             Console.WriteLine("-------------------------------");
             aulas[0] = "Trabalhando com Arrays";
+            ImprimirForeach(aulas);
 
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"Aula Modelando ocorre pela primeira vez no índice: {Array.IndexOf(aulas, aulaModelando)}");
+            Console.WriteLine($"Aula Modelando ocorre pela última vez no índice: {Array.LastIndexOf(aulas, aulaModelando)}");
+
+            Console.WriteLine("-------------------------------");
+            Array.Reverse(aulas);
             Imprimir(aulas);
+
+            Console.WriteLine("-------------------------------");
+            Array.Reverse(aulas);
+            Imprimir(aulas);
+
+            Console.WriteLine("-------------------------------");
+            Array.Resize(ref aulas, 3);
+            ImprimirForeach(aulas);
+
+            Console.WriteLine("-------------------------------");
+            Array.Resize(ref aulas, 4);
+            ImprimirForeach(aulas);
+
+            Console.WriteLine("-------------------------------");
+            aulas[aulas.Length - 1] = "Conclusão";
+            ImprimirForeach(aulas);
+
+            Console.WriteLine("-------------------------------");
+            Array.Sort(aulas);
+            ImprimirFor(aulas);
+
+            Console.WriteLine("-------------------------------");
+            string[] copia = new string[2];
+            Array.Copy(aulas, 1, copia, 0, 2);
+            ImprimirForeach(copia);
+
+            Console.WriteLine("-------------------------------");
+            string[] clone = aulas.Clone() as string[];
+            ImprimirFor(clone);
+
+            Console.WriteLine("-------------------------------");
+            Array.Clear(clone, 1, clone.Length -1);
+            ImprimirForeach(clone);
 
             Console.ReadLine();
         }
@@ -44,6 +85,7 @@ namespace CSharpCollections
             Console.WriteLine(aulas[0]);
             Console.WriteLine(aulas[1]);
             Console.WriteLine(aulas[2]);
+            Console.WriteLine(aulas[3]);
         }
 
         private static void ImprimirForeach(string[] aulas)
