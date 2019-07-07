e48a66332388d8432204581e30e55f3dca3e9cd2
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-07-07 16:10:51 -0300

    Introdução às Listas.

--- Formações em Programação/C# Collections/Parte 1/CSharpCollections/CSharpCollections/Program.cs
+++ Formações em Programação/C# Collections/Parte 1/CSharpCollections/CSharpCollections/Program.cs
@@ -1,105 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharpCollections
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-
             string aulaIntro = "Introdução à coleções";
             string aulaModelando = "Modelando Classe Aula";
             string aulaSets = "Trabalhando com conjuntos";
 
-
-            //string[] aulas = new string[]
+            //List<string> aulas = new List<string>()
             //{
             //    aulaIntro,
-            //    aulaModelando,
             //    aulaModelando,
             //    aulaSets
             //};
 
-            string[] aulas = new string[4];
-            aulas[0] = aulaIntro;
-            aulas[1] = aulaModelando;
-            aulas[2] = aulaModelando;
-            aulas[3] = aulaSets;
+            List<string> aulas = new List<string>();
+            aulas.Add(aulaIntro);
+            aulas.Add(aulaModelando);
+            aulas.Add(aulaSets);
+            ImprimirListForEach(aulas);
 
-            Imprimir(aulas);
-
+            Console.WriteLine($"A primeira Aula é: {aulas[0]}");
+            Console.WriteLine($"A primeira Aula é: {aulas.First()}");
+            Console.WriteLine($"A última Aula é: {aulas[aulas.Count - 1]}");
+            Console.WriteLine($"A última Aula é: {aulas.Last()}");
             Console.WriteLine("-------------------------------");
-            aulaIntro = "Trabalhando com Arrays";
-            ImprimirFor(aulas);
-
-            Console.WriteLine("-------------------------------");
-            aulas[0] = "Trabalhando com Arrays";
-            ImprimirForeach(aulas);
-
-            Console.WriteLine("-------------------------------");
-            Console.WriteLine($"Aula Modelando ocorre pela primeira vez no índice: {Array.IndexOf(aulas, aulaModelando)}");
-            Console.WriteLine($"Aula Modelando ocorre pela última vez no índice: {Array.LastIndexOf(aulas, aulaModelando)}");
-
-            Console.WriteLine("-------------------------------");
-            Array.Reverse(aulas);
-            Imprimir(aulas);
-
-            Console.WriteLine("-------------------------------");
-            Array.Reverse(aulas);
-            Imprimir(aulas);
-
-            Console.WriteLine("-------------------------------");
-            Array.Resize(ref aulas, 3);
-            ImprimirForeach(aulas);
-
-            Console.WriteLine("-------------------------------");
-            Array.Resize(ref aulas, 4);
-            ImprimirForeach(aulas);
-
-            Console.WriteLine("-------------------------------");
-            aulas[aulas.Length - 1] = "Conclusão";
-            ImprimirForeach(aulas);
-
-            Console.WriteLine("-------------------------------");
-            Array.Sort(aulas);
-            ImprimirFor(aulas);
-
-            Console.WriteLine("-------------------------------");
-            string[] copia = new string[2];
-            Array.Copy(aulas, 1, copia, 0, 2);
-            ImprimirForeach(copia);
-
-            Console.WriteLine("-------------------------------");
-            string[] clone = aulas.Clone() as string[];
-            ImprimirFor(clone);
-
-            Console.WriteLine("-------------------------------");
-            Array.Clear(clone, 1, clone.Length -1);
-            ImprimirForeach(clone);
 
             Console.ReadLine();
         }
 
-        private static void Imprimir(string[] aulas)
+        static void Imprimir(List<string> aulas)
         {
             Console.WriteLine(aulas[0]);
             Console.WriteLine(aulas[1]);
             Console.WriteLine(aulas[2]);
-            Console.WriteLine(aulas[3]);
+            Console.WriteLine("-------------------------------");
         }
 
-        private static void ImprimirForeach(string[] aulas)
+        static void ImprimirFor(List<string> aulas)
+        {
+            for (int i = 0; i < aulas.Count; i++)
+                Console.WriteLine(aulas[i]);
+            Console.WriteLine("-------------------------------");
+        }
+
+        static void ImprimirForEach(List<string> aulas)
         {
             foreach (var aula in aulas)
                 Console.WriteLine(aula);
+            Console.WriteLine("-------------------------------");
         }
 
-        private static void ImprimirFor(string[] aulas)
+        static void ImprimirListForEach(List<string> aulas)
         {
-            for (int i = 0; i < aulas.Length; i++)
-            {
-                Console.WriteLine(aulas[i]);
-            }
+            aulas.ForEach(aula => { Console.WriteLine(aula); });
+            Console.WriteLine("-------------------------------");
         }
+
     }
 }
