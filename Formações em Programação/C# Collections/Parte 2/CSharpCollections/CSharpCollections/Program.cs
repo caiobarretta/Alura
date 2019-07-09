3669636a1039acdbd814d3911c9589ebd34b8755
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-07-09 09:08:55 -0300

    SortedSet.

--- Formações em Programação/C# Collections/Parte 2/CSharpCollections/CSharpCollections/Program.cs
+++ Formações em Programação/C# Collections/Parte 2/CSharpCollections/CSharpCollections/Program.cs
@@ -7,13 +7,6 @@ namespace CSharpCollections
     {
         static void Main(string[] args)
         {
-            IDictionary<string, Aluno> sorted = new SortedDictionary<string, Aluno>();
-            sorted.Add("VT", new Aluno("Vanessa", 34672));
-            sorted.Add("AL", new Aluno("Ana", 5617));
-            sorted.Add("RN", new Aluno("Rafael", 17645));
-            sorted.Add("WM", new Aluno("Wanderson", 11287));
-            Imprimir(sorted);
-
             Console.ReadLine();
         }
     }
