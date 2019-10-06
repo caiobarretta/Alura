8dc14fec6473b967124e3236001fb24e914552f5
caiobarretta <caioabarretta@gmail.com>
2019-10-05 21:50:29 -0300

    Melhorando a lista

--- Formacao_Mobile/Android/Android_I_Crie_sua_App_fantastica_com_Android_Studio/app/src/main/java/com/example/agenda/MainActivity.java
+++ Formacao_Mobile/Android/Android_I_Crie_sua_App_fantastica_com_Android_Studio/app/src/main/java/com/example/agenda/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.agenda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] alunos = {"Daniel", "Ronaldo", "Jeferson", "Felipe"};
+        ListView lstAlunos = findViewById(R.id.lstAlunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        lstAlunos.setAdapter(adapter);
     }
 }
