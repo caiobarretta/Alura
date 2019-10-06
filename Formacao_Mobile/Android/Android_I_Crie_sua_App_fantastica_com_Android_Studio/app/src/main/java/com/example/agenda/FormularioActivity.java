da2029ee6a55c20776e3f92fd3dd626a77bcc219
caiobarretta <caioabarretta@gmail.com>
2019-10-05 23:37:25 -0300

    Navegando entre telas

--- Formacao_Mobile/Android/Android_I_Crie_sua_App_fantastica_com_Android_Studio/app/src/main/java/com/example/agenda/FormularioActivity.java
+++ Formacao_Mobile/Android/Android_I_Crie_sua_App_fantastica_com_Android_Studio/app/src/main/java/com/example/agenda/FormularioActivity.java
@@ -2,6 +2,7 @@ package com.example.agenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +21,7 @@ public class FormularioActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(FormularioActivity.this, "Botão Clicado!", Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
     }
--- Formacao_Mobile/Android/Android_I_Crie_sua_App_fantastica_com_Android_Studio/app/src/main/java/com/example/agenda/MainActivity.java
+++ Formacao_Mobile/Android/Android_I_Crie_sua_App_fantastica_com_Android_Studio/app/src/main/java/com/example/agenda/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.agenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -18,5 +21,14 @@ public class MainActivity extends AppCompatActivity {
         ListView lstAlunos = findViewById(R.id.lstAlunos);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
         lstAlunos.setAdapter(adapter);
+
+        Button novoAluno = findViewById(R.id.novo_aluno);
+        novoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FormularioActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
