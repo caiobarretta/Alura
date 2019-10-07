71eab60d90fef9380413e1649c69c1a6827f2ba1
caiobarretta <caioabarretta@gmail.com>
2019-10-06 22:14:39 -0300

    Trabalhando com outras informações da agenda

--- Formacao_Mobile/Android/Android_II_Integração_com_apps_e_recursos_do_device/app/src/main/java/com/example/agenda/MainActivity.java
+++ Formacao_Mobile/Android/Android_II_Integração_com_apps_e_recursos_do_device/app/src/main/java/com/example/agenda/MainActivity.java
@@ -74,13 +74,24 @@ public class MainActivity extends AppCompatActivity {
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
         final Aluno aluno =  (Aluno)lstAlunos.getItemAtPosition(info.position);
 
+        MenuItem itemSMS = menu.add("Enviar SMS");
+        Intent intentSMS = new Intent(Intent.ACTION_VIEW);
+        intentSMS.setData(Uri.parse("sms:"+aluno.getTelefone()));
+        itemSMS.setIntent(intentSMS);
+
+        MenuItem itemMapa = menu.add("Visualizar no mapa");
+        Intent intentMapa = new Intent(Intent.ACTION_VIEW);
+        intentMapa.setData(Uri.parse("geo:0,0?q=" + aluno.getEndereco()));
+        itemMapa.setIntent(intentMapa);
+
+
+        MenuItem itemSite = menu.add("Visitar Site");
+        Intent intentSite = new Intent(Intent.ACTION_VIEW);
         String site = aluno.getSite();
         if(!site.startsWith("http://")){
             site = "http://" + site;
         }
 
-        MenuItem itemSite = menu.add("Visitar Site");
-        Intent intentSite = new Intent(Intent.ACTION_VIEW);
         intentSite.setData(Uri.parse(site));
         itemSite.setIntent(intentSite);
 
