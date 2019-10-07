aaa4c90952e478092f0e820a4bfb03c92ad3523f
caiobarretta <caioabarretta@gmail.com>
2019-10-06 22:49:20 -0300

    Permissões no Android 6

--- Formacao_Mobile/Android/Android_II_Integração_com_apps_e_recursos_do_device/app/src/main/java/com/example/agenda/MainActivity.java
+++ Formacao_Mobile/Android/Android_II_Integração_com_apps_e_recursos_do_device/app/src/main/java/com/example/agenda/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.agenda;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Browser;
@@ -36,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         lstAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> lista, View item, int position, long id) {
-                Aluno aluno =  (Aluno)lstAlunos.getItemAtPosition(position);
+                Aluno aluno = (Aluno) lstAlunos.getItemAtPosition(position);
                 Intent intent = new Intent(MainActivity.this, FormularioActivity.class);
                 intent.putExtra("aluno", aluno);
                 startActivity(intent);
@@ -71,19 +74,39 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
-        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
-        final Aluno aluno =  (Aluno)lstAlunos.getItemAtPosition(info.position);
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        final Aluno aluno = (Aluno) lstAlunos.getItemAtPosition(info.position);
+
+        MenuItem itemLigar = menu.add("Ligar");
+
+        itemLigar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)
+                    != PackageManager.PERMISSION_GRANTED){
+
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[] {Manifest.permission.CALL_PHONE},
+                            123);
+                }
+                else{
+                    Intent intentLigar = new Intent(Intent.ACTION_CALL);
+                    intentLigar.setData(Uri.parse("tel:" + aluno.getTelefone()));
+                    startActivity(intentLigar);
+                }
+                return false;
+            }
+        });
 
         MenuItem itemSMS = menu.add("Enviar SMS");
         Intent intentSMS = new Intent(Intent.ACTION_VIEW);
-        intentSMS.setData(Uri.parse("sms:"+aluno.getTelefone()));
+        intentSMS.setData(Uri.parse("sms:" + aluno.getTelefone()));
         itemSMS.setIntent(intentSMS);
 
         MenuItem itemMapa = menu.add("Visualizar no mapa");
         Intent intentMapa = new Intent(Intent.ACTION_VIEW);
         intentMapa.setData(Uri.parse("geo:0,0?q=" + aluno.getEndereco()));
         itemMapa.setIntent(intentMapa);
-
 
         MenuItem itemSite = menu.add("Visitar Site");
         Intent intentSite = new Intent(Intent.ACTION_VIEW);
