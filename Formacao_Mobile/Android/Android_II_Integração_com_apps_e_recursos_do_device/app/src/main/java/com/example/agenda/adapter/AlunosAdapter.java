15a8c04a94272216e36fc2e0b395db68cfd7cd31
caiobarretta <caioabarretta@gmail.com>
2019-10-07 23:21:41 -0300

    Melhorias no layout

--- Formacao_Mobile/Android/Android_II_Integração_com_apps_e_recursos_do_device/app/src/main/java/com/example/agenda/adapter/AlunosAdapter.java
+++ Formacao_Mobile/Android/Android_II_Integração_com_apps_e_recursos_do_device/app/src/main/java/com/example/agenda/adapter/AlunosAdapter.java
@@ -1,12 +1,17 @@
 package com.example.agenda.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.agenda.MainActivity;
+import com.example.agenda.R;
 import com.example.agenda.modelo.Aluno;
 
 import java.util.List;
@@ -36,10 +41,31 @@ public class AlunosAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup viewGroup) {
-        TextView textView = new TextView(context);
+    public View getView(int position, View convertView, ViewGroup parent) {
         Aluno aluno = this.alunos.get(position);
-        textView.setText(aluno.toString());
-        return textView;
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = convertView;
+        if (view == null){
+            view = inflater.inflate(R.layout.list_item, parent, false);
+        }
+
+        TextView campoNome = view.findViewById(R.id.item_nome);
+        campoNome.setText(aluno.getNome());
+
+        TextView campoTelefone = view.findViewById(R.id.item_telefone);
+        campoTelefone.setText(aluno.getTelefone());
+
+        ImageView campoFoto = view.findViewById(R.id.item_foto);
+        String caminhoFoto = aluno.getCaminhoFoto();
+
+        if (caminhoFoto != null){
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 100, 100, true);
+            campoFoto.setImageBitmap(bitmapReduzido);
+            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+
+        return view;
     }
 }
