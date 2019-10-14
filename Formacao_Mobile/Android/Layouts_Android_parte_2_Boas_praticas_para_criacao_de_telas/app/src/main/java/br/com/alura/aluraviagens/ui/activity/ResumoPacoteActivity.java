54bf3f57853bbb4fc9f4157484dd8c63aee7b028
caioabarretta@gmail.com <caioabarretta@gmail.com>
2019-10-13 23:31:06 -0300

    Adicionando a data da viagem e titulo

--- Formacao_Mobile/Android/Layouts_Android_parte_2_Boas_praticas_para_criacao_de_telas/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.java
+++ Formacao_Mobile/Android/Layouts_Android_parte_2_Boas_praticas_para_criacao_de_telas/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.java
@@ -7,6 +7,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import br.com.alura.aluraviagens.R;
 import br.com.alura.aluraviagens.model.Pacote;
@@ -16,10 +18,15 @@ import br.com.alura.aluraviagens.util.ResourceUtil;
 
 public class ResumoPacoteActivity extends AppCompatActivity {
 
+    public static final String TITLE = "Resumo do pacote";
+    public static final String PATTERN_DATA = "dd/MM";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_pacote);
+
+        setTitle(TITLE);
 
         Pacote pacote = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal(243.99));
 
@@ -27,5 +34,18 @@ public class ResumoPacoteActivity extends AppCompatActivity {
         ((TextView)findViewById(R.id.resumo_pacote_local)).setText(pacote.getLocal());
         ((TextView)findViewById(R.id.resumo_pacote_dias)).setText(DiasUtil.formataEmTexto(pacote.getDias()));
         ((TextView)findViewById(R.id.resumo_pacote_preco)).setText(MoedaUtil.formataMoeda(pacote.getPreco()));
+
+        Calendar dataIda = Calendar.getInstance();
+        Calendar dataVolta = Calendar.getInstance();
+        dataVolta.add(Calendar.DATE, pacote.getDias());
+
+        SimpleDateFormat dateFormatBr = new SimpleDateFormat(PATTERN_DATA);
+        String dataFormatIda = dateFormatBr.format(dataIda.getTime());
+        String dataFormatVolta = dateFormatBr.format(dataVolta.getTime());
+
+        String dataFormatadaDaViagem = String.format("%s - %s de %d", dataFormatIda, dataFormatVolta, dataVolta.get(Calendar.YEAR));
+
+        ((TextView)findViewById(R.id.resumo_pacote_data)).setText(dataFormatadaDaViagem);
+
     }
 }
