aa743502d29e789961fced1d6646c1870d0d4197
caioabarretta@gmail.com <caioabarretta@gmail.com>
2019-10-15 23:23:17 -0300

    Refatorando o código Java

--- Formacao_Mobile/Android/Layouts_Android_parte_2_Boas_praticas_para_criacao_de_telas/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.java
+++ Formacao_Mobile/Android/Layouts_Android_parte_2_Boas_praticas_para_criacao_de_telas/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.java
@@ -7,11 +7,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 import br.com.alura.aluraviagens.R;
 import br.com.alura.aluraviagens.model.Pacote;
+import br.com.alura.aluraviagens.util.DataUtil;
 import br.com.alura.aluraviagens.util.DiasUtil;
 import br.com.alura.aluraviagens.util.MoedaUtil;
 import br.com.alura.aluraviagens.util.ResourceUtil;
@@ -19,7 +18,6 @@ import br.com.alura.aluraviagens.util.ResourceUtil;
 public class ResumoPacoteActivity extends AppCompatActivity {
 
     public static final String TITLE = "Resumo do pacote";
-    public static final String PATTERN_DATA = "dd/MM";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,23 +27,33 @@ public class ResumoPacoteActivity extends AppCompatActivity {
         setTitle(TITLE);
 
         Pacote pacote = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal(243.99));
+        mostrarImagem(pacote);
+        mostrarLocal(pacote);
+        mostrarDias(pacote);
+        mostrarPreco(pacote);
+        mostraData(pacote);
 
-        ((ImageView)findViewById(R.id.resumo_pacote_imagem)).setImageDrawable(ResourceUtil.devolveDrawable(this, pacote.getImagem()));
-        ((TextView)findViewById(R.id.resumo_pacote_local)).setText(pacote.getLocal());
-        ((TextView)findViewById(R.id.resumo_pacote_dias)).setText(DiasUtil.formataEmTexto(pacote.getDias()));
+    }
+
+    private void mostraData(Pacote pacote) {
+        ((TextView)findViewById(R.id.resumo_pacote_data)).setText(DataUtil.periodoEmTexto(pacote.getDias()));
+    }
+
+
+
+    private void mostrarPreco(Pacote pacote) {
         ((TextView)findViewById(R.id.resumo_pacote_preco)).setText(MoedaUtil.formataMoeda(pacote.getPreco()));
+    }
 
-        Calendar dataIda = Calendar.getInstance();
-        Calendar dataVolta = Calendar.getInstance();
-        dataVolta.add(Calendar.DATE, pacote.getDias());
+    private void mostrarDias(Pacote pacote) {
+        ((TextView)findViewById(R.id.resumo_pacote_dias)).setText(DiasUtil.formataEmTexto(pacote.getDias()));
+    }
 
-        SimpleDateFormat dateFormatBr = new SimpleDateFormat(PATTERN_DATA);
-        String dataFormatIda = dateFormatBr.format(dataIda.getTime());
-        String dataFormatVolta = dateFormatBr.format(dataVolta.getTime());
+    private void mostrarLocal(Pacote pacote) {
+        ((TextView)findViewById(R.id.resumo_pacote_local)).setText(pacote.getLocal());
+    }
 
-        String dataFormatadaDaViagem = String.format("%s - %s de %d", dataFormatIda, dataFormatVolta, dataVolta.get(Calendar.YEAR));
-
-        ((TextView)findViewById(R.id.resumo_pacote_data)).setText(dataFormatadaDaViagem);
-
+    private void mostrarImagem(Pacote pacote) {
+        ((ImageView)findViewById(R.id.resumo_pacote_imagem)).setImageDrawable(ResourceUtil.devolveDrawable(this, pacote.getImagem()));
     }
 }
