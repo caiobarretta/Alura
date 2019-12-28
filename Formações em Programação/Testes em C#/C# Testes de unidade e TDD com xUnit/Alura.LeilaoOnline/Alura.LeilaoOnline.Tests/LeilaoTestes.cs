b0414fe6877444a291de99cedbe4ccc617cd3c27
caiob@outoffthebox.tech <caiob@outoffthebox.tech>
2019-12-28 17:05:39 -0300

    A biblioteca XUnit

---
+++ Formações em Programação/Testes em C#/C# Testes de unidade e TDD com xUnit/Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -0,0 +1,56 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoTestes
+    {
+        [Fact]
+        public void LeilaoComUmLance()
+        {
+            //Arrange - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            leilao.RecebeLance(fulano, 800);
+
+            //Act - Método sobre test
+            leilao.TerminaPregao();
+            var ganhador = leilao.Ganhador;
+
+            //Assert
+            var valorEsperado = 800;
+            var valorObtido = ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        public void LeilaoComVariosLances()
+        {
+            //Arrange - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            leilao.RecebeLance(fulano, 800);
+
+            var maria = new Interessada("Maria", leilao);
+            leilao.RecebeLance(maria, 900);
+
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 990);
+
+
+            //Act - Método sobre test
+            leilao.TerminaPregao();
+            var ganhador = leilao.Ganhador;
+
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+    }
+}
