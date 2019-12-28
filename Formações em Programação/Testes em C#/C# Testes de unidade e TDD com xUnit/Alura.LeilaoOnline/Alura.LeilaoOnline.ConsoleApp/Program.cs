fb37a78fc7768284d5120d8aecbad134beb614ba
caiob@outoffthebox.tech <caiob@outoffthebox.tech>
2019-12-28 16:45:42 -0300

    Estrutura de um teste automatizado

--- Formações em Programação/Testes em C#/C# Testes de unidade e TDD com xUnit/Alura.LeilaoOnline/Alura.LeilaoOnline.ConsoleApp/Program.cs
+++ Formações em Programação/Testes em C#/C# Testes de unidade e TDD com xUnit/Alura.LeilaoOnline/Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -7,6 +7,47 @@ namespace Alura.LeilaoOnline.ConsoleApp
     {
         static void Main()
         {
+            LeilaoComVariosLances();
+            LeilaoComUmLance();
+        }
+
+        private static void Verifica(int valorEsperado, double valorObtido)
+        {
+            var cor = Console.ForegroundColor;
+            if (valorEsperado == valorObtido)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Teste Ok");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Teste Falhou. Esperado: {valorEsperado}, Obtido: {valorObtido}");
+            }
+            Console.ForegroundColor = cor;
+        }
+
+        private static void LeilaoComUmLance()
+        {
+            //Arrange - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            leilao.RecebeLance(fulano, 800);
+
+            //Act - Método sobre test
+            leilao.TerminaPregao();
+            var ganhador = leilao.Ganhador;
+
+            //Assert
+            var valorEsperado = 900;
+            var valorObtido = ganhador.Valor;
+
+            Verifica(valorEsperado, valorObtido);
+        }
+
+        private static void LeilaoComVariosLances()
+        {
+            //Arrange - cenário
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
             leilao.RecebeLance(fulano, 800);
@@ -18,10 +59,15 @@ namespace Alura.LeilaoOnline.ConsoleApp
             leilao.RecebeLance(maria, 990);
 
 
+            //Act - Método sobre test
             leilao.TerminaPregao();
             var ganhador = leilao.Ganhador;
 
-            Console.WriteLine(ganhador.Valor);
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = ganhador.Valor;
+
+            Verifica(valorEsperado, valorObtido);
         }
     }
 }
