5cf42d710adf9c8be5e22031684eba38f85e24cc
caiob@outoffthebox.tech <caiob@outoffthebox.tech>
2019-12-28 19:53:38 -0300

    Teorizando com xUnit

--- Formações em Programação/Testes em C#/C# Testes de unidade e TDD com xUnit/Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
+++ Formações em Programação/Testes em C#/C# Testes de unidade e TDD com xUnit/Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -26,107 +26,28 @@ namespace Alura.LeilaoOnline.Tests
             Assert.Equal(valorEsperado, valorObtido);
         }
 
-        [Fact]
-        public void LeilaoComTresClientes()
-        {
-            //Arrange - cenário
-            //Dado leilão com 3 clientes e lances realizados por eles
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            leilao.RecebeLance(fulano, 800);
-
-            var maria = new Interessada("Maria", leilao);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(maria, 990);
-
-            leilao.RecebeLance(fulano, 1000);
-
-            var beltrano = new Interessada("Beltrano", leilao);
-            leilao.RecebeLance(beltrano, 1400);
-
-            //Act - Método sobre test
-            //Quando o leilão termina
-            leilao.TerminaPregao();
-            var ganhador = leilao.Ganhador;
-
-            //Assert
-            //Então o valor esperado é o maior valor dado
-            // e o cliente ganhador é oque deu o maior lance
-            var valorEsperado = 1400;
-            var valorObtido = ganhador.Valor;
-
-            Assert.Equal(valorEsperado, valorObtido);
-            Assert.Equal(beltrano, ganhador.Cliente);
-        }
-
-        [Fact]
-        public void LeilaoComLancesOrdenadosPorValor()
+        [Theory]
+        [InlineData(1200, new double[] { 800, 900, 1000, 1200 })]
+        [InlineData(1000, new double[] { 800, 900, 1000, 990 })]
+        [InlineData(800, new double[] { 800 })]
+        public void LeilaoComVariosLances(double valorEsperado, double[] ofertas)
         {
             //Arrange - cenário
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
-            leilao.RecebeLance(fulano, 800);
-
-            var maria = new Interessada("Maria", leilao);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(maria, 990);
-
-            leilao.RecebeLance(fulano, 1000);
+            foreach (var oferta in ofertas)
+            {
+                leilao.RecebeLance(fulano, oferta);
+            }
 
             //Act - Método sobre test
             leilao.TerminaPregao();
             var ganhador = leilao.Ganhador;
 
             //Assert
-            var valorEsperado = 1000;
             var valorObtido = ganhador.Valor;
-
             Assert.Equal(valorEsperado, valorObtido);
         }
 
-        [Fact]
-        public void LeilaoComUmLance()
-        {
-            //Arrange - cenário
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            leilao.RecebeLance(fulano, 800);
-
-            //Act - Método sobre test
-            leilao.TerminaPregao();
-            var ganhador = leilao.Ganhador;
-
-            //Assert
-            var valorEsperado = 800;
-            var valorObtido = ganhador.Valor;
-
-            Assert.Equal(valorEsperado, valorObtido);
-        }
-
-        [Fact]
-        public void LeilaoComVariosLances()
-        {
-            //Arrange - cenário
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            leilao.RecebeLance(fulano, 800);
-
-            var maria = new Interessada("Maria", leilao);
-            leilao.RecebeLance(maria, 900);
-
-            leilao.RecebeLance(fulano, 1000);
-            leilao.RecebeLance(maria, 990);
-
-
-            //Act - Método sobre test
-            leilao.TerminaPregao();
-            var ganhador = leilao.Ganhador;
-
-            //Assert
-            var valorEsperado = 1000;
-            var valorObtido = ganhador.Valor;
-
-            Assert.Equal(valorEsperado, valorObtido);
-        }
     }
 }
