4bb7ff5c0cb4969700ab8f5cccb34cb2da856619
Caio Augusto Barretta <caioabarretta@gmail.com>
2020-01-01 17:07:35 -0300

    Concluindo o teste

--- Formações em Programação/Testes em C#/Selenium WebDriver e C# parte 2 Outros recursos/Alura.LeilaoOnline.Selenium/PageObjects/NovoLeilaoPO.cs
+++ Formações em Programação/Testes em C#/Selenium WebDriver e C# parte 2 Outros recursos/Alura.LeilaoOnline.Selenium/PageObjects/NovoLeilaoPO.cs
@@ -19,6 +19,8 @@ namespace Alura.LeilaoOnline.Selenium.PageObjects
         private By inputInicioPregrao;
         private By inputTerminoPregao;
 
+        private By inputBtnSalvar;
+
         public NovoLeilaoPO(IWebDriver driver)
         {
             _driver = driver;
@@ -27,9 +29,10 @@ namespace Alura.LeilaoOnline.Selenium.PageObjects
             inputDescricao = By.Id("Descricao");
             inputCategoria = By.Id("Categoria");
             inputValorInicial = By.Id("ValorInicial");
-            inputImagem = By.Id("Imagem");
+            inputImagem = By.Id("ArquivoImagem");
             inputInicioPregrao = By.Id("InicioPregao");
             inputTerminoPregao = By.Id("TerminoPregao");
+            inputBtnSalvar = By.CssSelector("button[type=submit]");
         }
 
         public void NavigateGoToUrl()
@@ -41,11 +44,18 @@ namespace Alura.LeilaoOnline.Selenium.PageObjects
         {
             _driver.FindElement(inputTitulo).SendKeys(titulo);
             _driver.FindElement(inputDescricao).SendKeys(descricao);
-            _driver.FindElement(inputCategoria).SendKeys(categoria);
+            
+            //_driver.FindElement(inputCategoria).SendKeys(categoria);
+
             _driver.FindElement(inputValorInicial).SendKeys(valorInicial.ToString());
             _driver.FindElement(inputImagem).SendKeys(imagem);
-            _driver.FindElement(inputInicioPregrao).SendKeys(inicioPregrao.ToString());
-            _driver.FindElement(inputTerminoPregao).SendKeys(terminoPregao.ToString());
+            _driver.FindElement(inputInicioPregrao).SendKeys(inicioPregrao.ToString("dd/MM/yyyy"));
+            _driver.FindElement(inputTerminoPregao).SendKeys(terminoPregao.ToString("dd/MM/yyyy"));
+        }
+
+        public void SubmitForm()
+        {
+            _driver.FindElement(inputBtnSalvar).Submit();
         }
     }
 }
