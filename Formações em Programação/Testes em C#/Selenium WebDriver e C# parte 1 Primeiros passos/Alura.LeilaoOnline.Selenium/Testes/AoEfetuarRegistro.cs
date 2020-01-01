90f279b3935b86d08b1c2791ea97124804f6de84
Caio Augusto Barretta <caioabarretta@gmail.com>
2019-12-31 23:02:55 -0300

    Registro inválido

--- Formações em Programação/Testes em C#/Selenium WebDriver e C# parte 1 Primeiros passos/Alura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
+++ Formações em Programação/Testes em C#/Selenium WebDriver e C# parte 1 Primeiros passos/Alura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
@@ -45,5 +45,40 @@ namespace Alura.LeilaoOnline.Selenium.Testes
             //Assert
             Assert.Contains("Obrigado", _driver.PageSource);
         }
+
+        [Theory]
+        [InlineData("", "caioabarretta@gmail.com", "123", "123")]
+        [InlineData("Caio Augusto Barretta", "", "123", "123")]
+        [InlineData("Caio Augusto Barretta", "caioabarretta", "123", "123")]
+        [InlineData("Caio Augusto Barretta", "caioabarretta@gmail.com", "", "123")]
+        [InlineData("Caio Augusto Barretta", "caioabarretta@gmail.com", "123", "")]
+        public void DadoInfoInvalidasDeveIrContinuarNaHome(string nome, string email, string password, string confirmPassword)
+        {
+            //Arrage
+            _driver.Navigate().GoToUrl("http://localhost:5000");
+
+            //Nome
+            var inputNome = _driver.FindElement(By.Id("Nome"));
+            inputNome.SendKeys(nome);
+
+            //Email
+            var inputEmail = _driver.FindElement(By.Id("Email"));
+            inputEmail.SendKeys(email);
+
+            //Password
+            var inputPassword = _driver.FindElement(By.Id("Password"));
+            inputPassword.SendKeys(password);
+
+            //ConfirmPassword
+            var inputConfirmPassword = _driver.FindElement(By.Id("ConfirmPassword"));
+            inputConfirmPassword.SendKeys(confirmPassword);
+
+            //Action
+            var btnRegistro = _driver.FindElement(By.Id("btnRegistro"));
+            btnRegistro.Click();
+
+            //Assert
+            Assert.Contains("section-registro", _driver.PageSource);
+        }
     }
 }
