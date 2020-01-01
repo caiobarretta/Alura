d8bfef3139a6999286bf0b3787738f834f6e1a13
Caio Augusto Barretta <caioabarretta@gmail.com>
2019-12-31 22:43:33 -0300

    Obtendo elementos

--- Formações em Programação/Testes em C#/Selenium WebDriver e C# parte 1 Primeiros passos/Alura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
+++ Formações em Programação/Testes em C#/Selenium WebDriver e C# parte 1 Primeiros passos/Alura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
@@ -16,14 +16,34 @@ namespace Alura.LeilaoOnline.Selenium.Testes
             _driver = fixture.Driver;
         }
 
-        public void Teste()
+        [Fact]
+        public void DadoInfoValidasDeveIrParaPaginaDeAgradecimento()
         {
             //Arrage
             _driver.Navigate().GoToUrl("http://localhost:5000");
 
+            //Nome
+            var inputNome = _driver.FindElement(By.Id("Nome"));
+            inputNome.SendKeys("Caio Augusto Barretta");
+
+            //Email
+            var inputEmail = _driver.FindElement(By.Id("Email"));
+            inputEmail.SendKeys("caioabarretta@gmail.com");
+
+            //Password
+            var inputPassword = _driver.FindElement(By.Id("Password"));
+            inputPassword.SendKeys("123");
+
+            //ConfirmPassword
+            var inputConfirmPassword = _driver.FindElement(By.Id("ConfirmPassword"));
+            inputConfirmPassword.SendKeys("123");
+
             //Action
+            var btnRegistro = _driver.FindElement(By.Id("btnRegistro"));
+            btnRegistro.Click();
 
             //Assert
+            Assert.Contains("Obrigado", _driver.PageSource);
         }
     }
 }
