8015ed82a0fb16533e3527b8e44603105d35b7ad
Caio Augusto Barretta <caioabarretta@gmail.com>
2020-01-02 20:01:23 -0300

    Dando lances

---
+++ Formações em Programação/Testes em C#/Selenium WebDriver e C# parte 2 Outros recursos/Alura.LeilaoOnline.Selenium/Testes/AoOfertarUmLance.cs
@@ -0,0 +1,45 @@
+﻿using Alura.LeilaoOnline.Selenium.Fixtures;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoOfertarUmLance
+    {
+        private readonly IWebDriver _driver;
+
+        private LoginPO loginPO;
+        private DetalheLeilaoPO detalheLeilaoPO;
+
+        public AoOfertarUmLance(TestFixture fixture)
+        {
+            _driver = fixture.Driver;
+
+            loginPO = new LoginPO(_driver);
+            detalheLeilaoPO = new DetalheLeilaoPO(_driver);
+        }
+
+        [Fact]
+        public void DadoLoginInteressadaDeveAtualizarLanceAtual()
+        {
+            //Arrange
+            loginPO.NavigateGoToUrl();
+            loginPO.PreencheFormulario("fulano@example.org", "123");
+            loginPO.SubmitForm();
+
+            detalheLeilaoPO.NavigateGoToUrl(1);
+
+            //Act
+            detalheLeilaoPO.OfertaLance(300);
+
+
+            //Assert
+            Assert.Equal(300, detalheLeilaoPO.LanceAtual);
+        }
+    }
+}
