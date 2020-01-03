9695a9be94ee7d75162b6a5da6bd077c95440314
Caio Augusto Barretta <caioabarretta@gmail.com>
2020-01-02 22:29:19 -0300

    Opções do navegador

---
+++ Formações em Programação/Testes em C#/Selenium WebDriver e C# parte 2 Outros recursos/Alura.LeilaoOnline.Selenium/PageObjects/MenuNaoLogadoPo.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+
+namespace Alura.LeilaoOnline.Selenium.PageObjects
+{
+    public class MenuNaoLogadoPo
+    {
+        private IWebDriver _driver;
+
+        public bool MenuMobileVisivel { get; }
+
+        public MenuNaoLogadoPo(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+    }
+}
---
+++ Formações em Programação/Testes em C#/Selenium WebDriver e C# parte 2 Outros recursos/Alura.LeilaoOnline.Selenium/Testes/AoNavegarParaHomeMobile.cs
@@ -0,0 +1,45 @@
+﻿using Alura.LeilaoOnline.Selenium.Fixtures;
+using Alura.LeilaoOnline.Selenium.Helpers;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoNavegarParaHomeMobile : IDisposable
+    {
+        private ChromeDriver driver;
+        private HomeNaoLogadaPO homeNaoLogadaPO;
+        public AoNavegarParaHomeMobile()
+        {
+            var deviceSettings = new ChromeMobileEmulationDeviceSettings();
+            deviceSettings.Width = 400;
+            deviceSettings.Height = 800;
+            deviceSettings.UserAgent = "Custom";
+
+            var options = new ChromeOptions();
+            options.EnableMobileEmulation(deviceSettings);
+            driver = new ChromeDriver(TestHelper.PastaDoExecutavel(), options);
+        }
+
+        [Fact]
+        public void DataLargura400DeveMostrarMenuMobile()
+        {
+            //Arrange
+            homeNaoLogadaPO = new HomeNaoLogadaPO(driver);
+
+            //Act
+            homeNaoLogadaPO.NavigateGoToUrl();
+
+            //Assert
+            Assert.True(homeNaoLogadaPO.Menu.MenuMobileVisivel);
+        }
+
+        public void Dispose() => driver.Quit();
+    }
+}
