a0fe1d2b84dac5ce9ad6ee3c3593fa522a7d780c
Caio Augusto Barretta <caioabarretta@gmail.com>
2020-06-28 16:21:13 -0300

    Implementando o avaliador de leilões

--- Formações em Programação/Python/Testes automatizados TDD com Python/testes-python-projeto-inicial/src/leilao/principal.py
+++ Formações em Programação/Python/Testes automatizados TDD com Python/testes-python-projeto-inicial/src/leilao/principal.py
@@ -1,5 +1,4 @@
-from src.leilao.dominio import Usuario, Lance, Leilao
-
+from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
 
 gui = Usuario('Gui')
 yuri = Usuario('Yuri')
@@ -8,8 +7,13 @@ lance_do_yuri = Lance(yuri, 100)
 lance_do_gui = Lance(gui, 150)
 
 leilao = Leilao('Celular')
-leilao.lances.append(lance_do_gui)
 leilao.lances.append(lance_do_yuri)
+leilao.lances.append(lance_do_gui)
 
 for lance in leilao.lances:
     print(f'O usuário {lance.usuario.nome} deu um lance de {lance.valor}')
+
+avalidador = Avaliador()
+avalidador.avalia(leilao)
+
+print(f'O menor lance foi de {avalidador.menor_lance} e o maior lance foi de {avalidador.maior_lance}')
