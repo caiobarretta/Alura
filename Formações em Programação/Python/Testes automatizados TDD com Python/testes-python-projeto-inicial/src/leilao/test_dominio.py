b40c75b8cb8267795327e3b938bee96680371488
Caio Augusto Barretta <caioabarretta@gmail.com>
2020-06-28 17:07:57 -0300

    Mais testes e classes de equivalência

--- Formações em Programação/Python/Testes automatizados TDD com Python/testes-python-projeto-inicial/src/leilao/test_dominio.py
+++ Formações em Programação/Python/Testes automatizados TDD com Python/testes-python-projeto-inicial/src/leilao/test_dominio.py
@@ -1,5 +1,4 @@
 from unittest import TestCase
-
 from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
 
 
@@ -41,6 +40,42 @@ class TestAvaliador(TestCase):
         avalidador.avalia(leilao)
         menor_valor_esperado = 100.0
         maior_valor_esperado = 150.0
+
+        self.assertEqual(menor_valor_esperado, avalidador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avalidador.maior_lance)
+
+    def test_deve_retornarn_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
+        gui = Usuario('Gui')
+
+        lance = Lance(gui, 150.0)
+
+        leilao = Leilao('Celular')
+        leilao.lances.append(lance)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        self.assertEqual(150.0, avaliador.menor_lance)
+        self.assertEqual(150.0, avaliador.maior_lance)
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_quando_o_leilao_tiver_tres_lances(self):
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
+        vini = Usuario ('Vini')
+
+        lance_do_yuri = Lance(yuri, 100.0)
+        lance_do_gui = Lance(gui, 150.0)
+        lance_do_vini = Lance(vini, 200.0)
+
+        leilao = Leilao('Celular')
+        leilao.lances.append(lance_do_yuri)
+        leilao.lances.append(lance_do_gui)
+        leilao.lances.append(lance_do_vini)
+
+        avalidador = Avaliador()
+        avalidador.avalia(leilao)
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 200.0
 
         self.assertEqual(menor_valor_esperado, avalidador.menor_lance)
         self.assertEqual(maior_valor_esperado, avalidador.maior_lance)
