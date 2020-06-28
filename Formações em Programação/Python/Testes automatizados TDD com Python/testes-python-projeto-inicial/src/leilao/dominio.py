ccd9e70c828df2d69ccab0e41311324cd512e781
Caio Augusto Barretta <caioabarretta@gmail.com>
2020-06-28 17:40:49 -0300

    Um pouco de encapsulamento

--- Formações em Programação/Python/Testes automatizados TDD com Python/testes-python-projeto-inicial/src/leilao/dominio.py
+++ Formações em Programação/Python/Testes automatizados TDD com Python/testes-python-projeto-inicial/src/leilao/dominio.py
@@ -23,10 +23,12 @@ class Leilao:
     def __init__(self, descricao):
         self.descricao = descricao
         self.__lances = []
+    def propoe(self, lance: Lance):
+        self.__lances.append(lance)
 
     @property
     def lances(self):
-        return self.__lances
+        return self.__lances[:]
 
 
 class Avaliador:
--- Formações em Programação/Python/Testes automatizados TDD com Python/testes-python-projeto-inicial/src/leilao/test_dominio.py
+++ Formações em Programação/Python/Testes automatizados TDD com Python/testes-python-projeto-inicial/src/leilao/test_dominio.py
@@ -13,8 +13,8 @@ class TestAvaliador(TestCase):
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
         yuri = Usuario('Yuri')
         lance_do_yuri = Lance(yuri, 100.0)
-        self.leilao.lances.append(lance_do_yuri)
-        self.leilao.lances.append(self.lance_do_gui)
+        self.leilao.propoe(lance_do_yuri)
+        self.leilao.propoe(self.lance_do_gui)
 
         avalidador = Avaliador()
         avalidador.avalia(self.leilao)
@@ -28,8 +28,8 @@ class TestAvaliador(TestCase):
         yuri = Usuario('Yuri')
         lance_do_yuri = Lance(yuri, 100.0)
 
-        self.leilao.lances.append(self.lance_do_gui)
-        self.leilao.lances.append(lance_do_yuri)
+        self.leilao.propoe(self.lance_do_gui)
+        self.leilao.propoe(lance_do_yuri)
 
         avalidador = Avaliador()
         avalidador.avalia(self.leilao)
@@ -40,7 +40,7 @@ class TestAvaliador(TestCase):
         self.assertEqual(maior_valor_esperado, avalidador.maior_lance)
 
     def test_deve_retornarn_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
-        self.leilao.lances.append(self.lance_do_gui)
+        self.leilao.propoe(self.lance_do_gui)
 
         avaliador = Avaliador()
         avaliador.avalia(self.leilao)
@@ -53,9 +53,9 @@ class TestAvaliador(TestCase):
         lance_do_yuri = Lance(yuri, 100.0)
         vini = Usuario('Vini')
         lance_do_vini = Lance(vini, 200.0)
-        self.leilao.lances.append(lance_do_yuri)
-        self.leilao.lances.append(self.lance_do_gui)
-        self.leilao.lances.append(lance_do_vini)
+        self.leilao.propoe(lance_do_yuri)
+        self.leilao.propoe(self.lance_do_gui)
+        self.leilao.propoe(lance_do_vini)
 
         avalidador = Avaliador()
         avalidador.avalia(self.leilao)
