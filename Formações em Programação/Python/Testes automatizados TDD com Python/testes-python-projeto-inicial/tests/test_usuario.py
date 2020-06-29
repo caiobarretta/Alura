f086c6c93b1a7f212b9c275daac9a1aebe141c73
Caio Augusto Barretta <caioabarretta@gmail.com>
2020-06-28 21:22:58 -0300

    Um pouco mais de testes

--- Formações em Programação/Python/Testes automatizados TDD com Python/testes-python-projeto-inicial/tests/test_usuario.py
+++ Formações em Programação/Python/Testes automatizados TDD com Python/testes-python-projeto-inicial/tests/test_usuario.py
@@ -5,5 +5,25 @@ def test_deve_subtrair_valor_da_carteira_do_usuario_quando_este_propor_um_lance(
     vini = Usuario('Vini', 100)
     leilao = Leilao('Celular')
     vini.propoe_lance(leilao, 50)
-
     assert vini.carteira == 50
+
+
+def test_deve_permitir_propor_lance_quando_o_valor_eh_menor_que_o_valor_da_carteira():
+    vini = Usuario('Vini', 100)
+    leilao = Leilao('Celular')
+    vini.propoe_lance(leilao, 1)
+    assert vini.carteira == 99
+
+
+def test_deve_permitir_propor_lance_quando_o_valor_eh_igual_ao_valor_da_carteira():
+    vini = Usuario('Vini', 100)
+    leilao = Leilao('Celular')
+    vini.propoe_lance(leilao, 100)
+    assert vini.carteira == 0
+
+
+def test_nao_deve_permitir_propor_lance_com_valor_maior_que_o_da_carteira():
+    vini = Usuario('Vini', 100)
+    leilao = Leilao('Celular')
+    vini.propoe_lance(leilao, 200)
+    assert vini.carteira == 100
