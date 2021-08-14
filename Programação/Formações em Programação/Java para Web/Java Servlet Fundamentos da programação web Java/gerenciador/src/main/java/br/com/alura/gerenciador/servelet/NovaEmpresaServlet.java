52073d08e07c4a5d5a77df3a3b87ec01b7278e83
Caio Augusto Barretta <caioabarretta@gmail.com>
2021-08-14 18:10:36 -0300

    Enviando parâmetros

---
+++ Programação/Formações em Programação/Java para Web/Java Servlet Fundamentos da programação web Java/gerenciador/src/main/java/br/com/alura/gerenciador/servelet/NovaEmpresaServlet.java
@@ -0,0 +1,32 @@
+package br.com.alura.gerenciador.servelet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NovaEmpresaServlet
+ */
+@WebServlet("/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		var nomeEmpresa =  request.getParameter("nome");
+		
+		var out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println(nomeEmpresa + " Empresa cadastrada com sucesso!");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
